0b76241cf84cee5f9e0000e290947c4b528f0fa8
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-18 00:54:13 -0600

    Create endpoints for user posts

---
+++ src/routes/posts.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const Post = require('../models/posts');
+const auth = require('../middleware/auth');
+const router = new express.Router();
+
+router.post('/posts', auth, async (req, res) => {
+	const post = new Post({
+		...req.body,
+		owner: req.user._id
+	});
+
+	try {
+		await post.save();
+		res.status(201).send(post);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+router.get('/posts', auth, async (req, res) => {
+	try {
+		const posts = await Post.find({ owner: req.user._id });
+		res.send(posts);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+router.get('/posts/:id', auth, async (req, res) => {
+	const _id = req.params.id;
+
+	try {
+		const post = await Post.findOne({ _id, owner: req.user._id });
+
+		if (!post) {
+			return res.status(404).send();
+		}
+
+		res.send(post);
+	} catch (e) {
+		res.status(404).send();
+	}
+});
+
+router.patch('/posts/:id', auth, async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = [ 'title', 'description', 'price' ];
+	const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
+
+	try {
+		const post = await Post.findOne({ _id: req.params.id, owner: req.user._id });
+
+		if (!post) {
+			return res.status(404).send();
+		}
+
+		updates.forEach(update => (task[update] = req.body[update]));
+		await post.save();
+		res.send(post);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+router.delete('/posts/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+
+		if (!post) {
+			return res.status(404).send();
+		}
+
+		res.send(post);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+module.exports = router;
