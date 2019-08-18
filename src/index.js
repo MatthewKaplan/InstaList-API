b875a6b209ef318d71e531ee7f34cea94a06a079
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-18 00:56:02 -0600

    Tell express to use the user and post routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('./db/mongoose');
+const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter);
+app.use(postRouter);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
